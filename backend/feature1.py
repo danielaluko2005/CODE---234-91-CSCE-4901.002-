3b2e5d7b72ea7532936b6d67bf512c097eca478b
Edidiong Gabriel <262231639+edigabe@users.noreply.github.com>
2026-09-24 09:11:23 -0500

    Update feature1.py

---
+++ backend/feature1.py
@@ -0,0 +1,11 @@
+#This is the implementation of the first feature 
+
+#Smart-Home Device Monitoring 
+
+#Parent Class for  devices (general attributes)
+
+#Child class for each kind of device 
+
+#Classes keep track of all the device data 
+
+#Flag for the state of machine (compromised vs. secure)
